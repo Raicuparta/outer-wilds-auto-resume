af5f8641ddbc265ac5d83c4679359090b3b89c88
Ricardo Lopes <raicuparta@gmail.com>
2020-01-10 23:06:50 +0100

    Initial commit

---
+++ AutoResume.cs
@@ -0,0 +1,30 @@
+﻿using OWML.ModHelper;
+using OWML.ModHelper.Events;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace OWML.AutoResume
+{
+    public class AutoResume : ModBehaviour
+    {
+        void Start() {
+            FindObjectOfType<TitleScreenAnimation>().SetValue("_fadeDuration", 0);
+            FindObjectOfType<TitleScreenAnimation>().SetValue("_gamepadSplash", false);
+            FindObjectOfType<TitleScreenAnimation>().SetValue("_introPan", false);
+            FindObjectOfType<TitleScreenManager>().Invoke("FadeInTitleLogo");
+
+
+            FindObjectOfType<TitleAnimationController>().SetValue("_logoFadeDelay", 0.001f);
+            FindObjectOfType<TitleAnimationController>().SetValue("_logoFadeDuration", 0.001f);
+            FindObjectOfType<TitleAnimationController>().SetValue("_optionsFadeDelay", 0.001f);
+            FindObjectOfType<TitleAnimationController>().SetValue("_optionsFadeDuration", 0.001f);
+            FindObjectOfType<TitleAnimationController>().SetValue("_optionsFadeSpacing", 0.001f);
+
+            Invoke("Resume", 0.5f);
+        }
+
+        void Resume() {
+            ExecuteEvents.Execute(GameObject.Find("Button-ResumeGame"), null, ExecuteEvents.submitHandler);
+        }
+    }
+}
